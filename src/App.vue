4629b614af178eafb9723dd17fba216a7940287e
andreas.groos <andreas.groos1@gmail.com>
2018-04-11 23:40:33 -0700

    boxgraph with highcharts-more

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,11 @@
 
 <script>
 import { Chart } from "highcharts-vue";
+import Highcharts from "highcharts";
+import HighchartsMore from "highcharts-more";
+HighchartsMore(Highcharts);
+// require("highcharts/modules/data")(Highcharts);
+// require('highcharts/modules/exporting')(Highcharts);
 
 export default {
   name: "App",
@@ -17,9 +22,78 @@ export default {
   data() {
     return {
       chartOptions: {
+        chart: {
+          type: "boxplot"
+        },
+
+        title: {
+          text: "Highcharts Box Plot Example"
+        },
+
+        legend: {
+          enabled: false
+        },
+
+        xAxis: {
+          categories: ["1", "2", "3", "4", "5"],
+          title: {
+            text: "Experiment No."
+          }
+        },
+
+        yAxis: {
+          title: {
+            text: "Observations"
+          },
+          plotLines: [
+            {
+              value: 932,
+              color: "red",
+              width: 1,
+              label: {
+                text: "Theoretical mean: 932",
+                align: "center",
+                style: {
+                  color: "gray"
+                }
+              }
+            }
+          ]
+        },
+
         series: [
           {
-            data: [1, 2, 3] // sample data
+            name: "Observations",
+            data: [
+              [760, 801, 848, 895, 965],
+              [733, 853, 939, 980, 1080],
+              [714, 762, 817, 870, 918],
+              [724, 802, 806, 871, 950],
+              [834, 836, 864, 882, 910]
+            ],
+            tooltip: {
+              headerFormat: "<em>Experiment No {point.key}</em><br/>"
+            }
+          },
+          {
+            name: "Outlier",
+            color: Highcharts.getOptions().colors[0],
+            type: "scatter",
+            data: [
+              // x, y positions where 0 is the first category
+              [0, 644],
+              [4, 718],
+              [4, 951],
+              [4, 969]
+            ],
+            marker: {
+              fillColor: "white",
+              lineWidth: 1,
+              lineColor: Highcharts.getOptions().colors[0]
+            },
+            tooltip: {
+              pointFormat: "Observation: {point.y}"
+            }
           }
         ]
       }

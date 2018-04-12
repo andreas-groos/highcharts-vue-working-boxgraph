54d2bfe40876c8969842a728adea7810c283b516
andreas.groos <andreas.groos1@gmail.com>
2018-04-11 23:29:34 -0700

    wokring graph with highcharts-vuegit add .git add .

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <highcharts :options="chartOptions"></highcharts>
+
+  </div>
+</template>
+
+<script>
+import { Chart } from "highcharts-vue";
+
+export default {
+  name: "App",
+  components: {
+    highcharts: Chart
+  },
+  data() {
+    return {
+      chartOptions: {
+        series: [
+          {
+            data: [1, 2, 3] // sample data
+          }
+        ]
+      }
+    };
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
